dcf4958ab9ccd0a24be18f0ed836170e4221b3ba
pangyuteng <pangyuteng@gmail.com>
2013-09-26 09:06:56 -0500

    added formatPathsForanalysis.m

--- getRetinalLayersExample.m
+++ getRetinalLayersExample.m
@@ -17,7 +17,6 @@
 %     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 %
 
-
 %   
 %
 %   This example script demonstrates the usage of CASEREL
@@ -128,7 +127,6 @@ display(sprintf('segmentation saved to %s',filename));
 %              retainl layers.
 
 close all;
-
 
 filename = [imagePath{1} '_octSegmentation.mat'];
 
