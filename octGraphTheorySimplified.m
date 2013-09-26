59964fd4a299aeec45e9c905de52a8a91ac9da73
pangyuteng <pangyuteng@gmail.com>
2013-09-26 00:16:35 -0500

    added gpl v2 license

--- octGraphTheorySimplified.m
+++ octGraphTheorySimplified.m
@@ -1,3 +1,23 @@
+% 
+%     {{Caserel}}
+%     Copyright (C) {{2013}}  {{Pangyu Teng}}
+% 
+%     This program is free software; you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation; either version 2 of the License, or
+%     (at your option) any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License along
+%     with this program; if not, write to the Free Software Foundation, Inc.,
+%     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%
+
+
 %% This script demonstrates how graph theory can be used to segment 
 % retinal layers in optical coherence tomography images. The method is based on 
 % 
